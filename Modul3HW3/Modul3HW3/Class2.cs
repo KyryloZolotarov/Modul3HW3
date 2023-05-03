2cfe5a43d2751f23e44afa604332075f10f39c47
Kyrylo Zolotarov <zlatashay@gmail.com>
2023-05-03 23:34:25 +0300

    done!

---
+++ Modul3HW3/Modul3HW3/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3HW3
+{
+    internal class Class2
+    {
+        private int z;
+        public Predicate<int> Calc(Func<int, int, int> funcPow, int x, int y)
+        {
+            z = funcPow(x, y);
+            return Result;
+        }
+
+        public bool Result(int x)
+        {
+            if (z % x == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Modul3HW3/Modul3HW3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Modul3HW3
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+            var class1 = new Class1();
+            var class2 = new Class2();
+            class1.ShowAction = Show;
+            var calc = class2.Calc(class1.Pow, 10, 20);
+            var result = calc.Invoke(10);
+            class1.ShowAction.Invoke(result);
+            void Show(bool result)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
